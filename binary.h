f34e92a7ba638f3db37063c079c549d4ebb1cee8
Sun Jia Cheng <69358106+sunjc826@users.noreply.github.com>
2024-09-01 04:07:41 +0000

    WIP: work on binary format

--- binary.h
+++ binary.h
@@ -1,31 +1,58 @@
 #include <base.h>
+#include <stdexcept>
 
 struct BinaryMagic 
 {
     std::array<NativeByte, "MIX_MAGIC"sv.size()> contents;
 };
 
-enum HeaderRecordType
+enum HeaderRecordType : NativeByte
 {
-    program_header_size,
-    program_header_offset,
-    entry_point,
+    hr_program_header_size,
+    hr_program_header_offset,
+    hr_entry_point,
+    hr_max,
 };
 
 struct HeaderRecord
 {
     HeaderRecordType type;
-    Word<OwnershipKind::owns> value;
+    Word<OwnershipKind::owns> word;
 };
 
 struct Header 
 {
-    
+    std::array<std::optional<HeaderRecord>, hr_max> records;
+
+    void check() const
+    {
+        for (size_t i = 0; i < records.size(); i++)
+        {
+            if (records[i])
+            {
+                if (records[i]->type != i)
+                    throw std::runtime_error("expected type to be equal to index");
+            }
+        }
+    }
+};
+
+enum ProgramHeaderRecordType : NativeByte
+{
+    phr_load,
+    phr_max,
+};
+
+struct ProgramHeaderRecord
+{
+    ProgramHeaderRecordType type;
+    Word<OwnershipKind::owns> offset;
+    Word<OwnershipKind::owns> size;  
 };
 
 struct ProgramHeader
 {
-
+    std::vector<ProgramHeaderRecord> records;
 };
 
 struct Binary
@@ -33,5 +60,5 @@ struct Binary
     BinaryMagic magic;
     Header header;
     ProgramHeader program_header;
-
+    NativeByte padding[main_memory_size];
 };
